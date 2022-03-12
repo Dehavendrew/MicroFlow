8773d23c29d9b342f185fc11a75fd58535dafa34
unknown <Drewhdehaven@outlook.com>
2022-03-12 12:52:14 -0600

    Failure and AKS

--- src/app/services/bluetooth.service.ts
+++ src/app/services/bluetooth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Session } from './session';
 import { DataService } from './data.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,17 @@ export class BluetoothService {
   packetSize = 250
   numMetrics = 2
   currentLoadPercent = 0
+  slidingWindowSize = 2
+  timeout = 2000
+
+  testFailureRate = 0.5
+  unAckedPackets: Number [] = []
+  sentAckedPackets: Number [] = []
+  
 
   constructor(private dataService: DataService) { }
+
+  
 
   async sendPacketTest(i): Promise<number[]>{
     var dataArray: number[] = []
@@ -20,17 +30,87 @@ export class BluetoothService {
       dataArray.push(32)
     }
 
-    return new Promise( resolve1 => {
-      setTimeout(resolve => {
-        resolve1(dataArray)}, 1000)
-    });
+    if (true){
+      return new Promise((resolve1, reject1) => {
+        setTimeout(resolve => {
+          this.unAckedPackets.push(i)
+          var thresh = Math.random()
+          if(thresh > this.testFailureRate){
+              resolve1(dataArray)
+          }
+          else{
+            console.log("LOST PACKET")
+            reject1("Lost Packet")
+          }
+        }, 1000)
+      });
+    }
+    else{
+      var msg = "Awating ACK " + this.unAckedPackets[0]
+      console.log(msg)
+      await this.awaitAckFromCentral().then((res) => {
+        console.log("Got Ack")
+        return new Promise((resolve1, reject1) => {
+          setTimeout(resolve => {
+            this.unAckedPackets.push(i)
+            var thresh = Math.random()
+            if(thresh > this.testFailureRate){
+              console.log(thresh)
+              resolve1(dataArray)
+            }
+            else{
+              console.log("LOST PACKET")
+              reject1("Lost Packet")
+            }
+            }, 1000)
+        });
+      }).catch((err) => console.log(err))
+    }
+  }
+
+  async awaitAckFromCentral(){
+    return new Promise((resolve, reject) => {
+      var counter = 0
+      console.log("Test")
+      while(this.unAckedPackets.length >= this.slidingWindowSize){
+        console.log(this.unAckedPackets.length)
+        if(counter > this.timeout){
+          console.log(counter)
+          reject("Timeout")
+          break
+        }
+        counter++
+      }
+      resolve
+    })
+  }
+
+  async ackFromCentral(i) {
+    var msg = "Preparing to send ACK " + i
+    console.log(msg)
+    var NumSecs = Math.floor(Math.random() * 3000)
+    return new Promise( resolve => {
+      setTimeout(resolve1 => {
+        this.sentAckedPackets.push(i)
+        for (let idx = 0; idx < this.unAckedPackets.length; idx++){
+          if(this.unAckedPackets[idx] == i){
+            this.unAckedPackets.splice(idx, 1);
+            var msg = "Sending ACK " + i
+            console.log(msg)
+            resolve
+          }
+        }
+        resolve(msg)
+      }
+      , NumSecs) 
+      })
   }
 
   async requestDataStream(sess: Session): Promise<Session>{
     this.currentLoadPercent = 0
     var numPackets = Math.ceil(sess.numSamples / (this.packetSize / this.numMetrics))
     var recievedPackets = 0
-    var msg = "requested data stream: " + sess.id
+    var msg = "Requested data stream: " + sess.id
     console.log(msg)
 
     var summaryArray: number[] = []
@@ -38,15 +118,18 @@ export class BluetoothService {
     const average = (array) => array.reduce((a, b) => a + b) / array.length;
 
     for(let i = 0; i < numPackets; i++){
-      msg = "waiting for packet " + i
+      msg = "Requesting Packet " + i
       console.log(msg)
       await this.sendPacketTest(i).then((res) => {
+        msg = "Got Packet " + i
+        console.log(msg)
         summaryArray.push(average(res))
         this.dataService.addRawPacket({sessionID: sess.id, idx: i, data: res}).then((res) => {
           recievedPackets++
           this.currentLoadPercent = recievedPackets / numPackets
         })
-      })
+      }).catch(err => i = i - 1)
+      await this.ackFromCentral(i).then((res) => {console.log("Done acking")})
     }
 
     return new Promise(resolve => {
